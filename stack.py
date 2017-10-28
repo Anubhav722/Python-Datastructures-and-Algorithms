6c870e732b77171017dfa300bbe71366de46cc67
Anubhav722 <anubhav@launchyard.com>
2017-10-29 02:19:55 +0530

    basic implementation of the stack

---
+++ stack.py
@@ -0,0 +1,25 @@
+# Stack follows LIFO (Last In First Out). The first element is called base and last is called top.
+ # you can access any element in an array--not 
+ # true for a stack, since you can only deal with the element at its top.
+ # Further details here: 
+ # https://softwareengineering.stackexchange.com/questions/147645/what-is-the-difference-between-an-array-and-a-stack
+
+class Stack(object):
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
