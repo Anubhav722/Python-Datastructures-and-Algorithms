5699641ad43e78181bad7fc1784cd694f79ffac3
Anubhav722 <anubhav@launchyard.com>
2017-10-28 06:07:54 +0530

    correct indentation

--- dynamicarray.py
+++ dynamicarray.py
@@ -21,50 +21,50 @@ import ctypes
 
 class DynamicArray(object):
 
-	def __init__(self):
-		self.n = 0 # count of the no. of elements in the initial array
-		self.capacity = 1
-		self.A = self.make_array(self.capacity) # initial array
+    def __init__(self):
+        self.n = 0 # count of the no. of elements in the initial array
+        self.capacity = 1
+        self.A = self.make_array(self.capacity) # initial array
 
-	def __len__(self):
-		"""
-		Returns no. of elements in the array.
-		"""
-		return self.n
+    def __len__(self):
+        """
+        Returns no. of elements in the array.
+        """
+        return self.n
 
-	def __getitem__(self, k):
-		"""
-		Returns element at index k.
-		"""
-		if not 0 <= k < self.n:
-			return IndexError("K index out of bounds")
-		return self.A[k]
+    def __getitem__(self, k):
+        """
+        Returns element at index k.
+        """
+        if not 0 <= k < self.n:
+            return IndexError("K index out of bounds")
+        return self.A[k]
 
-	def append(self, element):
-		"""
-		Appends an element at the end of the array.
-		"""
-		if self.n == self.capacity:
-			self._resize(2*self.capacity) # 2x if the capacity isn't enough.
+    def append(self, element):
+        """
+        Appends an element at the end of the array.
+        """
+        if self.n == self.capacity:
+            self._resize(2*self.capacity) # 2x if the capacity isn't enough.
 
-		self.A[self.n] = element
-		self.n += 1
+        self.A[self.n] = element
+        self.n += 1
 
-	def _resize(self, new_capacity):
-		"""
-		Resize the array when no. of elements are equal to capacity.
-		Uses `B` as a temporary array. 
-		"""
-		B = self.make_array(new_capacity)
+    def _resize(self, new_capacity):
+        """
+        Resize the array when no. of elements are equal to capacity.
+        Uses `B` as a temporary array. 
+        """
+        B = self.make_array(new_capacity)
 
-		for k in range(self.n):
-			B[k] = self.A[k]
-		
-		self.A = B
-		self.capacity = new_capacity
+        for k in range(self.n):
+            B[k] = self.A[k]
+        
+        self.A = B
+        self.capacity = new_capacity
 
-	def make_array(self, new_capacity):
-		"""
-		Creates a new array in accordance to the new capacity of the new array.
-		"""
-		return (new_capacity * ctypes.py_object)()
+    def make_array(self, new_capacity):
+        """
+        Creates a new array in accordance to the new capacity of the new array.
+        """
+        return (new_capacity * ctypes.py_object)()
